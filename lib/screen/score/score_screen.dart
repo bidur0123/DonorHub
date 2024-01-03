c0cba415e6e62a4e1abd612488443a112b51e82f
Bidur Gupta <bidurgupta125@gmail.com>
2024-01-04 00:05:44 +0530

    quiz screen

--- lib/screen/score/score_screen.dart
+++ lib/screen/score/score_screen.dart
@@ -15,6 +15,59 @@ class ScoreScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.black38,
       ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                opacity: .4,
+
+                // colorFilter: new ColorFilter.mode(Colors.red.withOpacity(0.2), BlendMode.dstATop),
+                image: AssetImage('assets/icons/bg.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              const Spacer(flex: 3),
+              Text(
+                "Score",
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall
+                    ?.copyWith(color: kSecondaryColor),
+              ),
+              const Spacer(),
+              Text(
+                "${_qnController.numOfCorrectAns * 10}/${_qnController.questions.length * 10}",
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium
+                    ?.copyWith(color: kSecondaryColor),
+              ),
+              if (_qnController.numOfCorrectAns !=
+                  _qnController.questions.length)
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text(
+                      "You don't seem completely suitable for donating blood, you can talk to a doctor about this.",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall
+                          ?.copyWith(color: kSecondaryColor),
+                    ),
+                  ),
+                ),
+              const Spacer(flex: 3),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
