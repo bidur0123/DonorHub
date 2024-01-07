5e0f9f957cc3543539cf2fb08a5ae7a6ae51ec43
Bidur Gupta <bidurgupta125@gmail.com>
2024-01-07 23:48:10 +0530

    profile page

---
+++ lib/screen/profile_page/profil_page.dart
@@ -0,0 +1,275 @@
+import 'package:donor_hub/handler/providers.dart';
+import 'package:donor_hub/screen/Drawer_screen/donor_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:provider/provider.dart';
+
+// donor page
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<Providers>(context).user;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(35, 4, 4, 1.0),
+                Color.fromRGBO(171, 39, 39, 1.0),
+              ],
+              begin: FractionalOffset.bottomCenter,
+              end: FractionalOffset.topCenter,
+            ),
+          ),
+        ),
+        Scaffold(
+          drawer: const DonorDrawer(),
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+          ),
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 73),
+              child: Column(
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        AntDesign.arrowleft,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        AntDesign.logout,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'My\nProfile',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontFamily: 'Nisebuschgardens',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 22,
+                  ),
+                  SizedBox(
+                    height: height * 0.80,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: innerHeight * 0.72,
+                                width: innerWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    const SizedBox(
+                                      height: 90,
+                                    ),
+                                    Text(
+                                      user[0]['first_name'] ?? " ",
+                                      style: const TextStyle(
+                                        color: Color.fromRGBO(171, 39, 39, 1.0),
+                                        fontFamily: 'Nunito',
+                                        fontSize: 37,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Blood Type',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            const Text(
+                                              'A-',
+                                              style: TextStyle(
+                                                color: Color.fromRGBO(
+                                                    171, 39, 39, 1.0),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                            vertical: 8,
+                                          ),
+                                          child: Container(
+                                            height: 50,
+                                            width: 3,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(100),
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Date-of-birth',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              user[0]['dob'] ?? " ",
+                                              style: const TextStyle(
+                                                color: Color.fromRGBO(
+                                                    171, 39, 39, 1.0),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Address',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              user[0]['address'] ?? " ",
+                                              style: const TextStyle(
+                                                color: Color.fromRGBO(
+                                                    171, 39, 39, 1.0),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 25,
+                                            vertical: 8,
+                                          ),
+                                          child: Container(
+                                            height: 50,
+                                            width: 3,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(100),
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Phone ',
+                                              style: TextStyle(
+                                                color: Colors.grey[700],
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                            Text(
+                                              user[0]['phone'] ?? " ",
+                                              style: const TextStyle(
+                                                color: Color.fromRGBO(
+                                                    171, 39, 39, 1.0),
+                                                fontFamily: 'Nunito',
+                                                fontSize: 25,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 110,
+                              right: 20,
+                              child: Icon(
+                                AntDesign.setting,
+                                color: Colors.grey[700],
+                                size: 30,
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Image.asset(
+                                  'assets/images/u12-cropped.png',
+                                  width: innerWidth * 0.45,
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
